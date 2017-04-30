a2c80f9c37f89ce84b9c7477ac2e0311bbb70507
Josh Yap <joshua.c.yap@gmail.com>
2017-04-29 22:16:20 -0500

    adding jawsdb code

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "sushi_db"
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(procss.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "sushi_db"
+  });
 });
 
 // Make connection.
